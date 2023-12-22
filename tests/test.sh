884279711a396766edf8afe16ec29bd1eb897162
lilia <ferrouklilia@gmail.com>
2023-12-22 12:06:35 +0100

    logique a suivre pour la creation des tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,1 +1,6 @@
-#pour compiler et appeler les differents tests.c
+#pour chaque fichier completé, on cree un fichier testX.c 
+#ou on vas tester toutes ses fonctions
+#on l'ajoute dans le makefile puis executer ce script ./test.sh
+make
+echo "************* test1: *************\n"
+./testX
