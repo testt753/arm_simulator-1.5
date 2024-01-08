5882ece93a2e8200084a1b595b887b7b97bfbee6
Mohamed Chahour <mohamed.chahour@etu.univ-grenoble-alpes.fr>
2024-01-08 02:04:42 +0100

    exception

--- arm_exception.c
+++ arm_exception.c
@@ -40,6 +40,11 @@ int arm_exception(arm_core p, uint8_t exception) {
      *   such as my_putchar given as an example
      */
     if (exception == SOFTWARE_INTERRUPT) {
+        cpsr = arm_read_cpsr(p);
+        set_bits(cpsr, 4, 0, 0b10011);
+        clr_bit(cpsr, 5);
+        set_bit(cpsr, 7);
+        clr_bit(cpsr, 9);
         uint32_t value;
         uint32_t address = arm_read_register(p, 15);
         address -= 8;
@@ -62,6 +67,86 @@ int arm_exception(arm_core p, uint8_t exception) {
         arm_write_spsr(p, old_cpsr);
         arm_write_register(p, 14, old_pc);
         arm_write_register(p, 15, 0);
+    }else if(exception == UNDEFINED_INSTRUCTION){
+        cpsr = arm_read_cpsr(p);
+        set_bits(cpsr, 4, 0, 0b11011);
+        clr_bit(cpsr, 5);
+        set_bit(cpsr, 7);
+        clr_bit(cpsr, 9);
+        arm_write_cpsr(p, cpsr);
+        arm_write_spsr(p, old_cpsr);
+        arm_write_register(p, 14, old_pc - 4);
+        /*traitement PC*/
+        uint32_t instruction;
+        arm_read_word(p, old_pc-8, &instruction);
+        printf("Undefined instruction 0x%08x at 0x%08x\n", instruction, old_pc-8); 
+        /**/
+        arm_write_register(p, 15, arm_read_register(p, 14));
+        arm_write_cpsr(p, arm_read_spsr(p));
+
+    } else if(exception == PREFETCH_ABORT){
+        cpsr = arm_read_cpsr(p);
+        set_bits(cpsr, 4, 0, 0b10111);
+        clr_bit(cpsr, 5);
+        set_bit(cpsr, 7);
+        clr_bit(cpsr, 9);
+        arm_write_cpsr(p, cpsr);
+        arm_write_spsr(p, old_cpsr);
+        arm_write_register(p, 14, old_pc - 4);
+        /*traitement PC*/
+        return END_SIMULATION;
+        /**/
+        arm_write_register(p, 15, arm_read_register(p, 14) - 4);
+        arm_write_cpsr(p, arm_read_spsr(p));
+    } else if(exception == DATA_ABORT){
+        cpsr = arm_read_cpsr(p);
+        set_bits(cpsr, 4, 0, 0b10111);
+        clr_bit(cpsr, 5);
+        set_bit(cpsr, 7);
+        clr_bit(cpsr, 9);
+        arm_write_cpsr(p, cpsr);
+        arm_write_spsr(p, old_cpsr);
+        arm_write_register(p, 14, old_pc);
+        /*traitement PC*/
+        uint32_t instruction;
+        arm_read_word(p, old_pc-8, &instruction);
+        printf("DATA ABORT 0x%08x at 0x%08x\n", instruction, old_pc-8); 
+        /**/
+        arm_write_register(p, 15, arm_read_register(p, 14) - 4);
+        arm_write_cpsr(p, arm_read_spsr(p));
+    } else if(exception == INTERRUPT){
+        cpsr = arm_read_cpsr(p);
+        set_bits(cpsr, 4, 0, 0b10010);
+        clr_bit(cpsr, 5);
+        set_bit(cpsr, 7);
+        clr_bit(cpsr, 9);
+        arm_write_cpsr(p, cpsr);
+        arm_write_spsr(p, old_cpsr);
+        arm_write_register(p, 14, old_pc);
+        /*traitement PC*/
+        uint32_t instruction;
+        arm_read_word(p, old_pc-8, &instruction);
+        printf("IRQ\n"); 
+        /**/
+        arm_write_register(p, 15, arm_read_register(p, 14) - 4);
+        arm_write_cpsr(p, arm_read_spsr(p));
+    }else if(exception == FAST_INTERRUPT){
+        cpsr = arm_read_cpsr(p);
+        set_bits(cpsr, 4, 0, 0b10010);
+        clr_bit(cpsr, 5);
+        set_bit(cpsr, 6);
+        set_bit(cpsr, 7);
+        clr_bit(cpsr, 9);
+        arm_write_cpsr(p, cpsr);
+        arm_write_spsr(p, old_cpsr);
+        arm_write_register(p, 14, old_pc);
+        /*traitement PC*/
+        uint32_t instruction;
+        arm_read_word(p, old_pc-8, &instruction);
+        printf("FIQ\n"); 
+        /**/
+        arm_write_register(p, 15, arm_read_register(p, 14) - 4);
+        arm_write_cpsr(p, arm_read_spsr(p));
     }
     return exception;
 }
